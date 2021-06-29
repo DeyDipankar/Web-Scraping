b716dece75dca257f26815d11a4d11d31f41ff94
DeyDipankar <deydipankar826@gmail.com>
2021-06-29 15:51:35 +0530

    updated hyperlinks,quotes & titles crawler

--- Scrapy/quotewebcrawler/quotewebcrawler/spiders/title_spider.py
+++ Scrapy/quotewebcrawler/quotewebcrawler/spiders/title_spider.py
@@ -1,11 +1,13 @@
 import scrapy
 
 class quotespider(scrapy.Spider):
-    name = 'quotes'
+    name = 'titles'
     start_urls = [
         "http://quotes.toscrape.com/"
     ]
 
     def parse(self, response):
         title = response.css('title::text').extract() #note the syntax "title::text"
+        #we can also use response.css('title::text').extract_first() instead of response.css('title::text')[0]
+        #to get the first element, difference is extract_first() returns None ifno item is found -this is an advantage
         yield ({'title_txt':title})
