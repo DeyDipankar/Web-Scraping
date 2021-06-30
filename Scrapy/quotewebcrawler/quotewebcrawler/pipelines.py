4a6617ea56a867f91fa549348f3fbbc89fcdbcc9
DeyDipankar <deydipankar826@gmail.com>
2021-06-30 18:58:41 +0530

    sqlite3 database integrated

--- Scrapy/quotewebcrawler/quotewebcrawler/pipelines.py
+++ Scrapy/quotewebcrawler/quotewebcrawler/pipelines.py
@@ -6,8 +6,37 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import sqlite3
 
+class QuotewebcrawlerPipeline(object):
 
-class QuotewebcrawlerPipeline:
+    def __init__(self):
+        self.create_connection()
+        self.create_table()
+
+    def create_connection(self):
+        """
+            This method will create the database connection & the cusror object
+        """
+        self.conn = sqlite3.connect("my_db.db")
+        self.cursor = self.conn.cursor()
+    
+    def create_table(self):
+        self.cursor.execute(""" DROP TABLE IF EXISTS my_table""")
+        self.cursor.execute(""" CREATE TABLE my_table (
+                                Quote text,
+                                Author text,
+                                Tag text)"""
+                            )
+
     def process_item(self, item, spider):
+        #print(item['quote'])
+        self.store_db(item)
         return item
+
+    def store_db(self,item):
+        #pass
+        self.cursor.execute(""" INSERT INTO my_table VALUES(?,?,?)""",(item['quote'][0],item['author'][0],
+                                                                            item['tag'][0])
+                            )
+        self.conn.commit()
--- Scrapy/quotewebcrawler/quotewebcrawler/settings.py
+++ Scrapy/quotewebcrawler/quotewebcrawler/settings.py
@@ -62,9 +62,10 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'quotewebcrawler.pipelines.QuotewebcrawlerPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'quotewebcrawler.pipelines.QuotewebcrawlerPipeline': 300, #here 300 is the priority. The higher priority that item
+   #will be chosen first
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
